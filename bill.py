56e7200ad701f15bea6716c5b910dbe71aff53e9
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-03 09:43:40 +0530

    first commit

---
+++ bill.py
@@ -0,0 +1,24 @@
+while(True):
+    print("select the option")
+    print("1.tea")
+    print('2. juice')
+    print('3.coffee')
+    print('4.dosa')
+    print('5.biriyani')
+    print('6. generate bill')
+    print('7.exit')
+    choice=int(input("enter the choice"))
+    if(choice==1):
+        print("added tea")
+    elif(choice==2):
+        print("juice added")
+    elif(choice==3):
+        print("coffee added")
+    elif(choice==4):
+        print("dosa added")
+    elif(choice==5):
+        print("biriyani added")
+    elif(choice==6):
+        print("gnerate bill")
+    elif(choice==7):
+        break
