bc3e2071be0b91b4bdc429004cebecd73708d93e
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-03 10:07:10 +0530

    item added

--- bill.py
+++ bill.py
@@ -1,15 +1,23 @@
+import mysql.connector
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='hoteldb')
+mycursor = mydb.cursor()
+total=0
+item=[]
 while(True):
     print("select the option")
-    print("1.tea")
-    print('2. juice')
-    print('3.coffee')
-    print('4.dosa')
-    print('5.biriyani')
+    print("1.tea.....20")
+    print('2. juice....70')
+    print('3.coffee....15')
+    print('4.dosa....25')
+    print('5.biriyani...140')
     print('6. generate bill')
     print('7.exit')
     choice=int(input("enter the choice"))
     if(choice==1):
         print("added tea")
+        qty=int(input("enter the quandity"))
+        total=20*qty
+        item.append("tea x"+str(qty))
     elif(choice==2):
         print("juice added")
     elif(choice==3):
